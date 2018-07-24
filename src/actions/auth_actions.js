f4d72fffb5588979ec563e0ae5b23890d5bb3451
Hector Sanchez <hsanchez7934@gmail.com>
2018-07-24 17:39:56 -0600

    Implment facebook auth

--- src/actions/auth_actions.js
+++ src/actions/auth_actions.js
@@ -1,19 +1,48 @@
 import { AsyncStorage } from 'react-native';
-import { FACEBOOK_LOGIN_SUCCESS } from './types';
+import { Facebook } from 'expo';
+import apiKeys from '../apiKeys';
+
+import {
+  FACEBOOK_LOGIN_SUCCESS,
+  FACEBOOK_LOGIN_FAIL
+} from './types';
 
 //AsyncStorage.setItem('fb_token', token);
 //AsyncStorage.getItem('fb_token');
-const facebookLoginSuccessAction = (action) => ({
 
-})
+//Action Creators
+const facebookLoginSuccessActionCreator = loginToken => ({
+  type: FACEBOOK_LOGIN_SUCCESS,
+  payload: loginToken
+});
+
+const facebookLoginFailActionCreator = () => ({
+  type: FACEBOOK_LOGIN_FAIL
+});
 
 export const facebookLogin = () => async dispatch => {
   // eslint-disable-next-line
   let token = await AsyncStorage.getItem('fb_token');
   if (token) {
     // Dispatch an action saying FB login is done
-    dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
+    dispatch(facebookLoginSuccessActionCreator(token));
   } else {
     // Start up FB login process
+    doFacebookLogin(dispatch);
   }
+};
+
+//Helper functions
+const doFacebookLogin = async dispatch => {
+  // eslint-disable-next-line
+  let { type, token } = await Facebook.logInWithReadPermissionsAsync(apiKeys.facebookAppId, {
+    permissions: ['public_profile']
+  });
+
+  if (type === 'cancel') {
+    dispatch(facebookLoginFailActionCreator());
+  }
+
+  await AsyncStorage.setItem('fb_token', token);
+  dispatch(facebookLoginSuccessActionCreator(token));
 };
